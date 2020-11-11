f7db2ac2629d082f69d1229c658c5ca11304c230
Armando <rojas.armani@gmail.com>
2020-11-11 07:56:52 -0500

    commit tree 5dcb01a4011845c93f475575ae952489afbe1501

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |number|
+    squared = number * number
+    squared_array << squared
+  end
+  return squared_array
 end
